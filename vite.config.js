94dca4a98a912ab6edd17d6a326132e65d167a59
Usman Yousuf <52995752+m-usman-yousuf@users.noreply.github.com>
2025-02-17 15:10:22 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-      'import.meta.env.VITE_CLIENT_ID': JSON.stringify(process.env.VITE_CLIENT_ID),  
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),    
-      'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY), }
+  'process.env': process.env
 })
