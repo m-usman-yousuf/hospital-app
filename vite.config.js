7daba9a6b7085dc2a9d74d3ac17c18597fe8e48d
Areesha Ahmed <areesha.ahmed@mazikglobal.com>
2025-02-17 15:19:04 +0500

    Secret key issue

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-      'import.meta.env.VITE_CLIENT_ID': JSON.stringify(process.env.VITE_CLIENT_ID),  
-      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),    
-      'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY), }
 })
